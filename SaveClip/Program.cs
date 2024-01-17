c9b5e8f20cdfc62ac88e8364817e159e61a7b949
Adrian Sims <adriansims62781@gmail.com>
2024-01-17 08:09:12 +0100

    First Commit

---
+++ SaveClip/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace SaveClip;
+
+internal static class Extensions
+{
+    public static void RunSta(this Thread thread)
+    {
+        thread.SetApartmentState(ApartmentState.STA);
+        thread.Start();
+        thread.Join();
+    }
+}
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+
+            IDataObject content = null;
+            new Thread(() =>
+            {
+                content = Clipboard.GetDataObject();
+                string[] formats = content.GetFormats();
+                foreach (string format in formats)
+                {
+                    Console.WriteLine(format);
+                }
+                Image i = Clipboard.GetImage();
+                i.Save("cb.png");
+            }
+            ).RunSta();
+
+            Console.WriteLine("got");
+
+        }
+        catch (Exception ex)
+        {
+            var fullname = System.Reflection.Assembly.GetEntryAssembly().Location;
+            var progname = Path.GetFileNameWithoutExtension(fullname);
+            Console.Error.WriteLine($"{progname} Error: {ex.Message}");
+        }
+    }
+}
